36fb4cce9d3c15295c4f3a050897052688f832fa
Rumman Islam <140172488+rummanislam@users.noreply.github.com>
2023-07-21 19:17:56 +0600

    Delete ex2.c

--- ex2.c
+++
@@ -1,9 +0,0 @@
-//conditional operator:
-#include<stdio.h>
-main()
-{
-    int a,b,max;
-    printf("Enter two numbers:\n");
-    scanf("%d%d",&a,&b);
-    max=(a>b)?(printf("Max =%d",a)):(printf("Max =%d",b));
-}
