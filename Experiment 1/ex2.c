1688e4abbfa8fd58f1b00f39a9608a75eccce118
Rumman Islam <140172488+rummanislam@users.noreply.github.com>
2023-07-21 19:28:28 +0600

    Add files via upload

---
+++ Experiment 1/ex2.c
@@ -0,0 +1,10 @@
+//a program to add two numbers:
+#include<stdio.h>
+main()
+{
+    int a,b,sum;
+    a=5;
+    b=7;
+    sum=a+b;
+    printf("The sum of %d and %d =%d",a,b,sum);
+}
---
+++ Experiment 1/pr2.c
@@ -0,0 +1,12 @@
+// area of a circle
+#include<stdio.h>
+main()
+{
+    int a;
+    float area;
+    printf("Enter the radius:\n");
+    scanf("%d",&a);
+    area=(3.1416*a*a);
+    printf("The area of the circle is :%.2f sq unit",area);
+
+}
