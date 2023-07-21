88095aba41ea87868b3de4badaedeb8ab34136fd
Rumman Islam <140172488+rummanislam@users.noreply.github.com>
2023-07-21 19:23:10 +0600

    Delete Experiment_1 directory

--- Experiment_1/pr4.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-main()
-{
-    float c,f;
-    printf("Enter the temperature in celcius:");
-    scanf("%f",&c);
-    f=((9*c)/5)+32;
-    printf("%.2f celsius = %.2f fahrenheit",c,f);
-}
--- Experiment_1/pr7.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-main()
-{
-    int a,b;
-    printf("Enter a positive integer:");
-    scanf("%d",&a);
-    (a%2==0)?(printf("%d is an even number",a)):(printf("%d is a odd number",a));
-}
